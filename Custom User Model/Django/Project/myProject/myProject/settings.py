b22e69b4affaa11447f101ae5fc0147d3b2c0800
nguyenductrong1207 <131521216+nguyenductrong1207@users.noreply.github.com>
2024-10-17 10:27:16 +0700

    update

--- Custom User Model/Django/Project/myProject/myProject/settings.py
+++ Custom User Model/Django/Project/myProject/myProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'myProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')], # HP 10 Lesson 4
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,3 +126,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 AUTH_USER_MODEL = "users.CustomUser"
+
+
+# Login & Logout
+LOGIN_REDIRECT_URL = 'home' # HP 10 L4
+LOGOUT_REDIRECT_URL = 'home' # HP 10 L4
